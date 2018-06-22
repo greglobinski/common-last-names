c6abe5f34192b8bba0e8000702cb214030c62c71
greglobinski <greglobinski@gmail.com>
2018-06-22 12:43:11 +0200

    test(jest): install jest instead mocha
    uninsatll mocha and chai, install jest and update test sripts

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import uniqueRandomArray from 'unique-random-array';
+
 const commonLastNames = require('./common-last-names.json');
 
 const mainExport = {
-  all: commonLastNames,
-  random: uniqueRandomArray(commonLastNames)
+    all: commonLastNames,
+    random: uniqueRandomArray(commonLastNames)
 };
 
 export default mainExport;
--- src/index.test.js
+++ src/index.test.js
@@ -1,20 +1,19 @@
-import { expect } from 'chai';
 import commonLastNames from '.';
 
 describe('common-last-names', () => {
-  it('should have a list of all available names', () => {
-    expect(commonLastNames.all).to.satisfy(isArrayOfStrings);
-  });
+    it('should have a list of all available names', () => {
+        expect(isArrayOfStrings(commonLastNames.all)).toBe(true);
+    });
 
-  it('should allow me to get a random name from the list', () => {
-    expect(commonLastNames.random()).to.satisfy(isIncludedIn(commonLastNames.all));
-  });
+    it('should allow me to get a random name from the list', () => {
+        expect(isIncludedIn(commonLastNames.all)(commonLastNames.random())).toBe(true);
+    });
 });
 
 function isArrayOfStrings(array) {
-  return array.every(i => typeof i === 'string');
+    return array.every(i => typeof i === 'string');
 }
 
 function isIncludedIn(array) {
-  return item => array.includes(item);
+    return item => array.includes(item);
 }
